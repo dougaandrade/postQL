b1ee99e1f925e52a65e75ba031ec81e235ee966f
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2025-06-13 11:35:22 -0300

    first commit

---
+++ src/main/java/com/dev/the_libs/TheLibsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.the_libs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLibsApplication.class, args);
+	}
+
+}
