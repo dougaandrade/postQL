b66bd30321b87001a5093355204450a552fe70ba
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2025-06-17 12:16:04 -0300

    perf: :zap:

--- src/main/java/com/dev/the_libs/model/Post.java
+++ src/main/java/com/dev/the_libs/model/Post.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Post {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
 
   private String title;
